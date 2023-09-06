a07098547b49a9ebd43da102a501aa3348b3b5af
caotruc <caoquattrat@gmail.com>
2023-09-06 16:14:47 +0700

    first commit

---
+++ Project1/Project1/thuattoan.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int search_first(vector<int>& a, int n, int l, int r, int x) {
+	int res = -1;
+	while (l <= r) {
+		int m = (l + r) / 2;
+		if (a[m] == x) {
+			res = m;
+			r = m - 1;
+		}
+		else if (a[m] > x) r = m - 1;
+		else l = m + 1;
+	}
+	return res;
+}
+
+int search_last(vector<int>& a, int n, int l, int r, int x) {
+	int res = -1;
+	while (l <= r) {
+		int m = (l + r) / 2;
+		if (a[m] == x) {
+			res = m;
+			l = m + 1;
+		}
+		else if (a[m] > x) r = m - 1;
+		else l = m + 1;
+	}
+	return res;
+}
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end()); // Sắp xếp vector a
+
+	long long cnt = 0;
+	for (int i = 0; i < n; i++) {
+		int x = k - a[i];
+		int first = search_first(a, n, i + 1, n - 1, x);
+		int last = search_last(a, n, i + 1, n - 1, x);
+		while (first != -1) {
+			first = search_first(a, n, i + 1, n - 1, x); // Cập nhật first
+			last = search_last(a, n, i + 1, n - 1, x);  // Cập nhật last
+			cnt += (last - first + 1);
+		}
+	}
+	cout << cnt << endl;
+
+	return 0;
+}
---
+++ thuattoannhiphan/thuattoannhiphan/Source.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+int gcd(int a, int b) {
+    while (b) {
+        a = a % b;
+        std::swap(a, b);
+    }
+    return a;
+}
+
+std::vector<int> largestDivisors(const std::vector<int>& arr) {
+    std::vector<int> result;
+    for (int num : arr) {
+        int largestDivisor = 1;
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                largestDivisor = std::max(largestDivisor, std::max(i, num / i));
+            }
+        }
+        result.push_back(largestDivisor);
+    }
+    return result;
+}
+
+int main() {
+    std::vector<int> arr = { 3, 20, 9 };
+    std::vector<int> result = largestDivisors(arr);
+
+    std::cout << "Output: ";
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
